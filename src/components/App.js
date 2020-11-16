4698d48e7c6d087061ad8dce212a03b3e0c61f3e
Necralma <lihuel.almada@gmail.com>
2020-11-16 11:43:48 -0300

    Added folders in src

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Home from './screens/Home/Home';
+
+function App() {
+  return (
+    <div className="wrapper">  
+        <div className="main">
+          <Home />
+        </div>
+    </div>
+  );
+}
+
+export default App;
